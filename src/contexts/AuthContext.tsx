744ab9848095d478aba7a79306b73dd2f17b49fb
Adam Ryšánek <adamrysanek@Adam-MacBook-Pro.local>
2021-11-23 22:59:02 +0100

    Add refreshToken to cookies

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -7,9 +7,13 @@ import React, {
 } from "react";
 import { auth } from "src/utils/auth/firebaseClient";
 import firebase from "firebase/compat/app";
+import { parseCookies, setCookie, destroyCookie } from "nookies";
 
 type User = firebase.User | null;
-type HandleAuthFunc = (email: string, password: string) => Promise<firebase.auth.UserCredential>;
+type HandleAuthFunc = (
+	email: string,
+	password: string
+) => Promise<firebase.auth.UserCredential>;
 type LogoutFunc = () => Promise<void>;
 type ResetPasswordFunc = (email: string) => Promise<void>;
 
@@ -33,17 +37,27 @@ export const AuthProvider: FC = ({ children }) => {
 	};
 	const handleLogin = (email: string, password: string) => {
 		return auth.signInWithEmailAndPassword(email, password);
-	}
+	};
 	const handleLogout = () => {
 		return auth.signOut();
-	}
+	};
 	const handleResetPassword = (email: string) => {
 		return auth.sendPasswordResetEmail(email);
-	}
+	};
 
 	useEffect(() => {
 		const unsubscribe = auth.onAuthStateChanged((user) => {
-			setUser(user);
+			if (user) {
+				setUser(user);
+				setCookie(null, "refreshToken", user.refreshToken);
+			}
+			if (!user) {
+				setUser(null);
+				const refreshToken = parseCookies()["refreshToken"];
+				if (refreshToken) {
+					destroyCookie(null, "refreshToken");
+				}
+			}
 		});
 
 		return unsubscribe;
