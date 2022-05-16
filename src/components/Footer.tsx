06f3a442482b70c719d1b406960d5cf67e82298d
Adam Ryšánek <adamrysanek@Adam-MacBook-Pro.local>
2022-05-16 17:12:11 +0200

    Stop using styled-components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,21 @@
+import { Box } from "@chakra-ui/react";
 import React from "react";
-import styled from "styled-components";
 
-const Wrapper = styled.footer`
-	width: 100%;
-	background: darkcyan;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	min-height: 10vh;
-	font-size: 16px;
-	line-height: 1.3;
-`;
-
-const Footer = () => <Wrapper>Footer</Wrapper>;
+const Footer = () => (
+	<Box
+		bg="blue.200"
+		sx={{
+			width: "100%",
+			display: "flex",
+			justifyContent: "center",
+			alignItems: "center",
+			padding: "16px 0",
+			fontSize: "16px",
+			lineHeight: "1.3",
+		}}
+	>
+		Footer
+	</Box>
+);
 
 export default Footer;
